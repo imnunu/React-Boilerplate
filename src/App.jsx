57b8b85eca665b6bada1e94abc7e971d60eaf01a
Ellen Chang <imnunu@gmail.com>
2017-04-04 21:11:10 +0000

    import ChatBar,MessageList,Message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar">
-          <a href="/" class="navbar-brand">Chatty</a>
-        </nav>
-        <h1>Hello React :)</h1>
+        <MessageList />
+        <Message />
+        <ChatBar />
       </div>
     );
   }
