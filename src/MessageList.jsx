559f5626d8e96d752caa5768cad4109b4f5b1bc6
Ellen Chang <imnunu@gmail.com>
2017-04-04 21:46:52 +0000

    rename Message to MessageList, and added Navbar.jsx

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,9 +3,15 @@ import React, {Component} from 'react';
 class MessageList extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
+      <main class="messages">
+        <div class="message">
+          <span class="message-username">Anonymous1</span>
+          <span class="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div class="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
     );
   }
 }
